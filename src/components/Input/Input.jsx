0f30015855097c05626df0564f216160d4ff5e4d
Mykhailo <myscreamo@gmail.com>
2025-04-15 14:31:25 +0200

    get tasks from fatch

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,13 @@
-const Input = () => {
+import classes from './Input.module.css'
+const Input = ({ type='text', onChange, value, placeholder }) => {
   return(
-    <input type="text" className={classes.input} onChange={handleSetValue} placeholder='Add task' value={value}/>
+    <input 
+      type="text" 
+      className={classes.input} 
+      onChange={onChange} 
+      placeholder={placeholder} 
+      value={value}
+    />
   )
 }
 export default Input
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Button from '../Button/Button'
 import Tabs from '../Tabs/Tabs'
 import classes from './Main.module.css'
 import ContentSection from '../ContentSection/ContentSection'
+import Input from '../Input/Input'
 const Main = () =>{
   const [inputValue, setInputValue] = useState('')
   const [ items, setItems] = useState([])
+  useEffect(() =>{
+
+    const getTask= async () =>{
+      try{
+        const res = await fetch('http://localhost:3000/todos');
+        if(!res.ok){
+          throw new Error('Can`t get all tasks')
+        }
+        return await res.JSON()
+      } catch(error) {
+        console.error(error.message)
+      }
+    }
+    getTask()
+  }, [])
+
   const handleClick = () =>{
     if(inputValue !== "") {
-      setItems([...items, inputValue])
+      setItems([...items, 
+        {
+          title: inputValue,
+          description: '',
+          completed: false
+        }
+      ])
       setInputValue('')
+      console.log(items)
     }
   }
-  
+
   return(
     <main className={classes.main}>
       <section className={classes.section}>
-        <input 
-          type="text" 
-          className={classes.input} 
-          onChange={e => setInputValue(e.target.value)} 
-          placeholder='Add task' 
+        <Input
+          type='text'
+          onChange={(e)  => setInputValue(e.target.value)}
+          placeholder={'Enter task'}
           value={inputValue}
         />
         <Button onClick={handleClick}>Add </Button>
       </section>
       <Tabs/>
       <ContentSection>
-        {items.map((item, index) => (<li key={index}>{item}</li>))}
+        {items.map((item, index) => (<li key={index}>{item.title}</li>))}
       </ContentSection>  
     </main>
   )
